952ab5628fdcd3734bdc382a23f973d58aa4462b
Alessandro <alessandrocosta103@gmail.com>
2024-06-20 03:49:22 -0300

    finaliza primeira versao

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Digite o valor que será convertido: ");
+        Scanner scan = new Scanner(System.in);
+        String valor = scan.nextLine();
+        System.out.println("Digite o código da moeda: ");
+        String moedaBase = scan.nextLine();
+        System.out.println("Agora informe para qual moeda este valor precisa ser convertido:");
+        String moedaConvertida =scan.nextLine();
+        System.out.println("");
+        String linkApi = "https://v6.exchangerate-api.com/v6/183f02f3f6692bccf61e29fd/pair/"+moedaBase+"/"+moedaConvertida+"/"+valor;
+        HttpClient cliente = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(linkApi)).build();
+        HttpResponse<String> response = cliente.send(request ,HttpResponse.BodyHandlers.ofString());
+        String json = response.body();
+        Gson gson = new Gson();
+        Moeda moedaRecord = gson.fromJson(json, Moeda.class);
+        Moedas moedas = new Moedas(moedaRecord);
+        System.out.println(moedaRecord);
+        System.out.println(moedas);
     }
 }
---
+++ src/Moedas.java
@@ -0,0 +1,23 @@
+public class Moedas {
+
+    private String base_cod;
+    private String target_code;
+    private double conversion_rate;
+    private double conversion_result;
+
+    public Moedas(Moeda moedaRecord) {
+        this.base_cod = moedaRecord.base_code();
+        this.target_code = moedaRecord.target_code();
+        this.conversion_rate = Double.valueOf(moedaRecord.conversion_rate());
+        this.conversion_result = Double.valueOf(moedaRecord.conversion_result());
+    }
+
+    @Override
+    public String toString() {
+        return "" +
+                "Codigo da moeda base: '" + base_cod + '\'' +
+                ", Codigo da moeda convertida: '" + target_code + '\'' +
+                ", Valor unitario da moeda: " + conversion_rate +
+                ", Valor convertido: " + conversion_result;
+    }
+}
